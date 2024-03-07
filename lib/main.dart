b67df493b5637df8923620ac2b47628db97b8530
DIANA_ESTEFANIA_SANCHEZ_SAUCEDO <a.20308051281195@cbtis128.edu.mx>
2024-03-07 03:01:52 +0000

    Actualizar

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiAppBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiAppBar extends StatelessWidget {
+  const MiAppBar({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: EjMiAppBar(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class EjMiAppBar extends StatelessWidget {
+  const EjMiAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
+        title: const Text("Ejemplo AppBar"),
+        centerTitle: true,
+        automaticallyImplyLeading: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.access_alarm),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.account_circle,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.more_vert,
+            ),
+            onPressed: () {},
+          )
+        ],
+        backgroundColor: Color(0xff43a848),
+        elevation: 8,
+        shadowColor: Colors.blueGrey,
+        shape: const StadiumBorder(),
+        toolbarHeight: 65,
+      ), // Fin de AppBar
+      drawer: const Drawer(),
+      body: const Center(
         child: Text(
-          'Hello, World!',
+          'Inicio Estefanía Sánchez',
+          style: TextStyle(fontSize: 24),
         ),
       ),
-    );
+    ); // Fin de Scaffold estructura
   }
 }
